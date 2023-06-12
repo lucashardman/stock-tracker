937a218d510a8d53384830354c7f613e20312be0
Lucas Hardman <lucas.hardman92@gmail.com>
2023-06-12 13:15:15 -0300

    finalização do mailer

--- infrastructure/operations.py
+++ infrastructure/operations.py
@@ -1,4 +1,8 @@
 import yfinance as yf
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+from dotenv import load_dotenv
+import os
 
 def fetch_stock_price(symbol):
     try:
@@ -15,3 +19,18 @@ def fetch_stock_price(symbol):
         }
 
 
+def send_email(subject, body):
+    try:
+        load_dotenv()
+        sg = SendGridAPIClient(str(os.getenv("SENDGRID_API_KEY")))
+        sg.send(
+            Mail(
+                from_email=str(os.getenv("EMAIL_SENDER")),
+                to_emails=str(os.getenv("EMAIL_SENDER")),
+                subject=subject,
+                plain_text_content=body
+            )
+        )
+        print('Email sent successfully!')
+    except Exception as e:
+        print('Error sending email:', str(e))
--- tracker/cron.py
+++ tracker/cron.py
@@ -1,4 +1,6 @@
+from hmac import new
 from infrastructure.repositories.meilisearch import Meilisearch
+from infrastructure.operations import send_email, fetch_stock_price
 import datetime
 
 def mailer():
@@ -20,11 +22,33 @@ def mailer():
         notification_expire_time = start_time + datetime.timedelta(
             minutes=int(dt.get("track_time", 0))
         )
+
+        stock_name = dt.get("name", "")
+        user = dt.get("user", "")
+
+        
+        old_price = float(dt.get("price", 0))
+
         if notification_time < now:
-            print(notification_time, now)
-            print("Enviando email para:")
-            print(dt)
-            meili.update(dt, {"last_check_at_dt": datetime.datetime.now().isoformat()})
             if notification_expire_time < now:
                 meili.remove(dt.get("id"))
+            else:
+                print(f"Enviando email para {user} sobre {stock_name}")
+                new_price = float(fetch_stock_price(stock_name).get("price", 0))
+
+                if new_price > float(dt.get("max_val", old_price)):
+                    send_email(
+                        subject=f"Aleração no preço da ação {stock_name}", 
+                        body=f'O preço da ação {stock_name} subiu para {new_price}, atingindo o valor máximo de {dt.get("max_val")}.'
+                    )
+                elif new_price < float(dt.get("min_val", old_price)):
+                    send_email(
+                        subject=f"Aleração no preço da ação {stock_name}", 
+                        body=f'O preço da ação {stock_name} desceu para {new_price}, atingindo o valor mínimo de {dt.get("min_val")}.'
+                    )
+                meili.update(dt, {
+                    "last_check_at_dt": datetime.datetime.now().isoformat(),
+                    "price": new_price
+                })
+            
 
